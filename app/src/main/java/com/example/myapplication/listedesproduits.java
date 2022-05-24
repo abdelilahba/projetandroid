20f3487941e2d707dc1f1e755188efea353b5dca
Stagiaire <abdelilah.bakkali258@gmail.com>
2022-05-24 18:08:50 +0100

    listedesproduits

--- app/src/main/java/com/example/myapplication/listedesproduits.java
+++ app/src/main/java/com/example/myapplication/listedesproduits.java
@@ -3,13 +3,28 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class listedesproduits extends AppCompatActivity {
+    ListView lst;
+    MyDBProduit db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listedesproduits);
+        db=new MyDBProduit(this);
+         ArrayList<produit> ps= MyDBProduit.getall(db.getReadableDatabase());
+         ArrayList<String> s=new ArrayList<>();
+         for(produit p : ps){
+             s.add(p.getId()+"  "+ p.getLibelle());
+         }
+         ArrayAdapter d =new ArrayAdapter(this,R.layout.activity_listedesproduits,s);
+         lst.setAdapter(d);
+
 
     }
 }
